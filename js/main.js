ce2c7565442bfdd0e0dae56a73e03a57037fff11
AnujxYadav <yanuj2002@gmail.com>
2024-04-13 21:28:07 +0530

    testing

--- js/main.js
+++ js/main.js
@@ -112,6 +112,10 @@ function submitHandler () {
     var firstName = document.querySelector('input[name="firstName"]').value;
     var emailID = document.querySelector('input[name="emailID"]').value;
     var mobileNumber = document.querySelector('input[name="mobileNumber"]').value;
+    if(mobileNumber.length !== 10) {
+        alert("Please enter a valid mobile number");
+        return;
+    }
     var billInfo = document.querySelector('select[name="billInfo"]').value;
     var specialNote = document.querySelector('textarea[name="specialNote"]').value;
     var preFilledLink = 'https://docs.google.com/forms/d/e/1FAIpQLSfLu4e4e0QGVS1-XwN9MVyJyP_vzfZNZtip7GPPq7VwCMmXCw/viewform?usp=pp_url&entry.1510382416=' + encodeURIComponent(firstName) + '&entry.1290999781=' + encodeURIComponent(emailID) + '&entry.786904787=' + encodeURIComponent(mobileNumber) + '&entry.26756016=' + encodeURIComponent(billInfo) + '&entry.1755035927=' + encodeURIComponent(specialNote);
@@ -119,3 +123,91 @@ function submitHandler () {
     // window.location.href = preFilledLink;
     window.open(preFilledLink, '_blank').focus(); 
 }
+
+const dict = {
+    'residential_content': `<p class="mt-4">Our residential solar panel services bring clean energy solutions to your doorstep. We specialize in custom installations tailored to your home's unique needs, maximizing energy efficiency and savings. Our expert team handles everything from design to installation with precision and professionalism, ensuring a seamless transition to solar power.</p>
+
+<p>With a focus on quality and reliability, we use industry-leading technology and materials to deliver durable and long-lasting solar systems. Whether you're looking to reduce your carbon footprint, lower your electricity bills, or gain energy independence, we're here to make it happen.</p>
+
+<p>Beyond installation, we provide ongoing maintenance and support to keep your solar panels performing at their best for years to come. Our commitment to customer satisfaction means you can trust us to deliver exceptional service every step of the way.</p>`,
+    'commercial_content': `<p>Elevate your business with our comprehensive commercial solar services. We specialize in designing and implementing customized solar solutions tailored to meet the unique energy needs of commercial enterprises. Our expert team works closely with you to assess your energy requirements, analyze your facility, and develop a solar system that maximizes efficiency and savings.</p>
+
+<p>From large-scale installations on rooftops to ground-mounted arrays, we leverage cutting-edge technology and high-quality materials to ensure optimal performance and longevity. Our commitment to excellence extends beyond installation – we provide ongoing maintenance and support to ensure your solar system operates at peak efficiency for years to come.</p>
+
+<p>By harnessing the power of the sun, you can significantly reduce your operating costs, enhance your sustainability profile, and contribute to a cleaner environment. Join the ranks of forward-thinking businesses embracing renewable energy solutions and take control of your energy future today.</p>`,
+    'industrial_content': `<p>Transform your industrial facility with our specialized industrial solar services. We excel in crafting tailor-made solar solutions engineered to meet the robust energy demands of industrial operations. Our dedicated team collaborates closely with you to assess your energy needs, conduct thorough site evaluations, and develop a solar system optimized for maximum efficiency and cost-effectiveness.</p>
+
+<p>Whether your facility requires rooftop installations, ground-mounted arrays, or solar carports, we leverage state-of-the-art technology and premium-grade materials to ensure robust performance and durability. Our end-to-end services encompass design, installation, and ongoing maintenance, guaranteeing seamless operation and prolonged system lifespan.</p>
+
+<p>By harnessing solar energy, you can substantially reduce operational expenses, bolster your sustainability credentials, and mitigate your carbon footprint. Join the forefront of industrial enterprises embracing renewable energy solutions and secure a competitive edge in today's green economy.</p>`
+}
+
+
+const org = {
+    'residential_content': `<p>Our residential solar panel services bring clean energy solutions to your
+                                        doorstep. We specialize in custom
+                                        installations tailored to your home's unique needs, maximizing energy efficiency
+                                        and savings.</p>
+                                    <p class="small fw-medium text-primary">Hover to Read More<i
+                                            class="fa fa-arrow-right ms-2"></i></p>`,
+    'commercial_content': `<p>Elevate your business with our comprehensive commercial solar services. Our
+                                        expert
+                                        team works closely to assess your
+                                        requirements, analyze your facility, and develop a solar system that maximizes
+                                        efficiency and savings.</p>
+                                    <p class="small fw-medium text-primary">Hover to Read More<i
+                                            class="fa fa-arrow-right ms-2"></i></p>`,
+    'industrial_content': `<p>Transform your industrial facility with our specialized industrial solar
+                                        services. We
+                                        excel in crafting tailor-made
+                                        solar solutions engineered to meet the robust energy demands of industrial
+                                        operations.</p>
+                                    <p class="small fw-medium text-primary">Hover to Read More<i
+                                            class="fa fa-arrow-right ms-2"></i></p>`
+}
+
+$(document).ready(function() {
+  var timeoutId;
+
+  $('.col-md-6.col-lg-4.wow.fadeInUp.view1').hover(
+    function() {
+      clearTimeout(timeoutId);
+      var that = $(this);
+      timeoutId = setTimeout(function() {
+        that.siblings().hide(0);
+        var child = that.children('.service-item').first();
+        child.css({
+          'display': 'flex',
+          'flex-direction': 'row'
+        });
+          var icon = child.children('.position-relative').first().children('.service-icon').first();
+          // hide the icon
+            icon.hide();
+          var nonHoverChild = child.children('.position-relative').first().children('.non-hover').first();
+          // change the content of the non-hover div to "Hello"
+            nonHoverChild.html(dict[nonHoverChild.attr('id')]);
+        that.animate({ width: '100%' }, 150);
+      }, 500); // 500 milliseconds = 0.5 second
+    },
+    function() {
+      var that = $(this);
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(function() {
+        var child = that.children('.service-item').first();
+        child.css({
+          'display': 'block',
+          'flex-direction': 'column'
+        });
+          var icon = child.children('.position-relative').first().children('.service-icon').first();
+          // hide the icon
+            icon.show();
+          var nonHoverChild = child.children('.position-relative').first().children('.non-hover').first();
+          // change the content of the non-hover div to "Hello"
+            nonHoverChild.html(org[nonHoverChild.attr('id')]);
+        that.animate({ width: '33.33%' }, 200, function() {
+          that.siblings().show(250);
+        });
+      }, 500); // 500 milliseconds = 0.5 second
+    }
+  );
+});
